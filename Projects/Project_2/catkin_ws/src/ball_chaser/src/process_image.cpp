e32363e3ed857627fb8bde5c955a783c6ec4f0f2
Ioannis <isouflas@gmail.com>
2019-10-26 21:54:08 +0100

    added functionality to chase ball

--- Projects/Project_2/catkin_ws/src/ball_chaser/src/process_image.cpp
+++ Projects/Project_2/catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -9,11 +9,16 @@ ros::ServiceClient client;
 ball_chaser::DriveToTarget srv;
 
 // This function calls the command_robot service to drive the robot in the specified direction
-void drive_robot(float lin_x, float ang_z)
+void drive_robot(float linear_x, float angular_z)
 {
     // TODO: Request a service and pass the velocities to it to drive the robot
-    srv.request.linear_x = lin_x;
-    srv.request.angular_z = ang_z;
+    srv.request.linear_x = linear_x;
+    srv.request.angular_z = angular_z;
+
+    // Call the drive_bot service and pass the requested velocities
+    if (!client.call(srv)) {
+    ROS_ERROR("Failed to call service drive_bot!");
+    }
 }
 
 // This callback function continuously executes and reads the image data
@@ -26,6 +31,46 @@ void process_image_callback(const sensor_msgs::Image img)
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
+
+    // look for ball
+    int ball_position = -1; 
+    for (int i = 0; i < img.height * img.step; i++) {
+    if (img.data[i] == white_pixel) {
+      ball_position = i % img.step; // expessed in ratio (0.5 is centre)
+      break;
+    } 
+    }
+
+    // control linear and angular velocities based on ball position
+    float linear_x = 0.0, angular_z = 0.0;
+    if (ball_position <= img.step * 0.3 && ball_position >= 0) 
+    { // turn left
+    angular_z = 0.5;
+    } 
+    else if (ball_position > img.step * 0.7  && ball_position <= img.step) 
+    { // turn right
+    angular_z = -0.5;
+    } 
+    else if (ball_position != -1) 
+    { // move forward
+    linear_x = 0.5;
+    }
+    else if (ball_position == -1) 
+    { // look around to find the ball - CAN BE FURTHER IMPROVED IN FUTURE
+    angular_z = 0.5;
+    }
+             
+    // send request to drive_robot service
+    drive_robot(linear_x, angular_z); 
+
+
+    // Publish some info
+  if (ball_position != -1) { 
+    ROS_INFO_STREAM("Target detected: driving the bot to the target ...");
+  } else {
+    ROS_INFO_STREAM("Target missing: Stop the bot for now.");
+  }
+
 }
 
 int main(int argc, char** argv)
