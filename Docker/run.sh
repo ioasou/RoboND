1d8585c4b7d30679778d9cc33138198a87988b90
ioasou <isouflas@gmail.com>
2019-11-16 21:07:42 +0000

    run docker using current user

--- Docker/run.sh
+++ Docker/run.sh
@@ -6,8 +6,8 @@ docker run -it --rm\
     --env=TERM="xterm-color"\
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="$HOME:/home/user:rw"\
-    --workdir="/home/user/workspace/RoboND"\
+    --volume="$HOME:/home/$USER:rw"\
+    --workdir="/home/$USER/workspace/RoboND"\
     --name="ros-kinetic-gazebo-container"\
     --privileged=True \
     ros-kinetic-gazebo $SHELL
