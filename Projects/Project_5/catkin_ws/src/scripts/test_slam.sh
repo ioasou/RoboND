8ef5281dc7fcd879d67f0796c717d6bc196023d1
ioasou <isouflas@gmail.com>
2019-12-28 19:04:48 +0000

    updated directory names

--- Projects/Project_5/catkin_ws/src/scripts/test_slam.sh
+++ Projects/Project_5/catkin_ws/src/scripts/test_slam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # setup gazebo world
-export TURTLEBOT_GAZEBO_WORLD_FILE=/home/user/workspace/RoboND/Projects/Project_5/catkin_ws/src/World/simple_u.world
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/user/workspace/RoboND/Projects/Project_5/catkin_ws/src/map/simple_u.world
 
 # start gazebo world
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" & 
