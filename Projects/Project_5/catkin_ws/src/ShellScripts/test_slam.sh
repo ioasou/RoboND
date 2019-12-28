234a8c7bca760df29dbbbf4da93ec182591a1c10
ioasou <isouflas@gmail.com>
2019-12-28 13:50:30 +0000

    test slam start up functionality

--- Projects/Project_5/catkin_ws/src/ShellScripts/test_slam.sh
+++ Projects/Project_5/catkin_ws/src/ShellScripts/test_slam.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 # setup gazebo world
 export TURTLEBOT_GAZEBO_WORLD_FILE=/home/user/workspace/RoboND/Projects/Project_5/catkin_ws/src/World/project_1_ball.world
 
 # start gazebo world
-roslaunch turtlebot_gazebo turtlebot_world.launch & /
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" & 
 
-sleep 5 && roslaunch turtlebot_gazebo gmapping_demo.launch
+# start gmapping
+sleep 5 && xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" & 
+
+# start visualisations (rviz)
+sleep 5 && xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" & 
+
+# start teleoperation
+sleep 5 && xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch"
