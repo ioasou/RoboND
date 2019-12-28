3227f7951b9f505420017557acabf401a9d8b0bd
ioasou <isouflas@gmail.com>
2019-12-28 18:01:07 +0000

    updated world so that is simpler - gmapping not sufficient for complicated worlds

--- Projects/Project_5/catkin_ws/src/ShellScripts/test_slam.sh
+++ Projects/Project_5/catkin_ws/src/ShellScripts/test_slam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # setup gazebo world
-export TURTLEBOT_GAZEBO_WORLD_FILE=/home/user/workspace/RoboND/Projects/Project_5/catkin_ws/src/World/project_1_ball.world
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/user/workspace/RoboND/Projects/Project_5/catkin_ws/src/World/simple_u.world
 
 # start gazebo world
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" & 
